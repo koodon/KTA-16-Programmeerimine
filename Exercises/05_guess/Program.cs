10a6dedaf81d08b1cbcc86dd93dbbf894b743886
koodon <koodon@mail.ee>
2017-10-23 15:33:37 +0300

    guess

--- Exercises/05_guess/Program.cs
+++ Exercises/05_guess/Program.cs
@@ -10,9 +10,22 @@ namespace _05_guess
     {
         static void Main(string[] args)
         {
+            int nr = 40;
             Console.WriteLine("Ma valin välja ühe suvalise numbri [1-100]. Proovi see ära arvata");
-            Console.WriteLine("Sina pakud: ");
+            Console.Write("Sina pakud: ");
+            int input = int.Parse(Console.ReadLine());            
 
+            if (input > nr)
+            {
+                Console.WriteLine("Sinu valitud number on suurem!");
+            }
+            if (nr > input)
+            {
+                Console.WriteLine("Sinu valitud number on väiksem!");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
         }
     }
 }
