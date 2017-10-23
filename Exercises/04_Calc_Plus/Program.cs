0ea97f72dd785ef01cce2509f0feac1a6bdd3c3e
koodon <koodon@mail.ee>
2017-10-23 14:59:54 +0300

    calcP

---
+++ Exercises/04_Calc_Plus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Calc_Plus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm. Ütle mulle 2 numbrit ja ma ütlen Sulle nende summa");
+            Console.Write("Esimene number: ");
+            string input = Console.ReadLine();
+            int arv1 = int.Parse(input);
+            Console.Write("Teine number: ");
+            string input2 = Console.ReadLine();
+            int arv2 = int.Parse(input2);
+            Console.WriteLine("Summaks on: " + (arv1 + arv2));
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Exercises/05_guess/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri [1-100]. Proovi see ära arvata");
+            Console.WriteLine("Sina pakud: ");
+
+        }
+    }
+}
