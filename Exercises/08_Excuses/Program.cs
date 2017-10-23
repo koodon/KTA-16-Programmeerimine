c25477100a278402f10c723fdeadb4e094d7ab41
koodon <koodon@mail.ee>
2017-10-23 15:47:44 +0300

    exc

---
+++ Exercises/08_Excuses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Excuses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutit kasutada", "Mingi tekst veel" };
+
+            for (int i = 0; i < excuses.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises/09_Excuses_Each/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Excuses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutit kasutada", "Mingi tekst veel" };
+
+            int i = 0;
+            foreach (var excuse in excuses)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
