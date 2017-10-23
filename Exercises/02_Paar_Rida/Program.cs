5edce89b31d0c3f1990805321b9c190b7f2d1084
koodon <koodon@mail.ee>
2017-10-23 14:26:02 +0300

    2r

---
+++ Exercises/02_Paar_Rida/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Paar_Rida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minu nimi on Hr.X ning õpin esimest korda programmeerima.");
+            Console.WriteLine("Kindlasti on võimalik midagi rohkemat, kuid lihtsalt teksti printimine.");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
