caea1c695300903de49151628b26a200601cfe99
koodon <koodon@mail.ee>
2017-10-23 14:37:11 +0300

    goi

---
+++ Exercises/03_Papagoi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Papagoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen papagoi programm ja ma kordan selle Sulle tagasi...");
+            Console.Write("Sina ütled: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Mina ütlen: " + input);
+            Console.ReadLine();
+        }
+    }
+}
