e326b945f861f77b41f43515610e7d80dabefdf9
koodon <koodon@mail.ee>
2017-11-20 23:23:56 +0200

    tk1_ed1

--- Exercises/00_Procent/Program.cs
+++ Exercises/00_Procent/Program.cs
@@ -10,15 +10,35 @@ namespace _00_Procent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dicount Calculator");
+            Console.WriteLine("Allahindluse kalkulaator");
+            Console.WriteLine();
+            Console.Write("Sisesta summa: ");
+            int input = int.Parse(Console.ReadLine());
 
-            while (true)
+            double value = 0;
+            double percent = 0;
+
+            if (input < 50 && input > 0)
             {
-                Console.WriteLine("Enter your discount: ");
-                int number = int.Parse(Console.ReadLine());
+                percent = 0;
             }
-
-            if (number < 50) ;
+            if (input > 50 && input < 250)
+            {
+                percent = 5;
+                value = input * 0.05;
+            }
+            if (input > 250 && input < 500)
+            {
+                percent = 10;
+                value = input * 0.10;
+            }
+            if (input > 500)
+            {
+                percent = 20;
+                value = input * 0.20;
+            }
+            Console.WriteLine($"Allahindluse arvutuskäik: {input} - {percent}% = {input - value}");
+            Console.ReadLine();
         }
     }
 }
